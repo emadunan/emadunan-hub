1e8615cb6a3fa3879c56cd73ca9c87e3b043d07c
emadunan <emadunan@gmail.com>
2025-07-24 13:22:29 +0300

    chore: Ignore error if emadunan-hub is not available on pm2

--- scripts/deployToProd.sh
+++ scripts/deployToProd.sh
@@ -4,7 +4,7 @@ set -e
 # Ensure Node.js tools are in the PATH
 export PATH=$HOME/.nvm/versions/node/v22.14.0/bin:$PATH
 
-pm2 delete emadunan-hub
+pm2 delete emadunan-hub || true
 
 npm run clean
 npm install
