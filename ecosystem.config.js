eb47af9667e108890be0b8c0829a60822ebff765
emadunan <emadunan@gmail.com>
2025-08-06 14:16:26 +0300

    chore: Add emadunan-work deploy configurations

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,5 +9,14 @@ module.exports = {
         PORT: 3030,
       },
     },
+    {
+      name: "emadunan-work",
+      script: "sh",
+      args: "-c 'npm run start'",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3033,
+      },
+    },
   ],
 };
