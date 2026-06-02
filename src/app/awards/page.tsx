8bba33ba2477e036a5b89984b9ffa6c2759f48ed
emadunan <emadunan@gmail.com>
2026-06-02 18:30:09 +0300

    feat: Add awards in a separate tab

---
+++ src/app/awards/page.tsx
@@ -0,0 +1,13 @@
+import AwardsList from '@/components/awards/AwardsList';
+import MainContainer from '@/components/layout/MainContainer'
+import React from 'react'
+
+const AwardsPage = () => {
+  return (
+    <MainContainer>
+      <AwardsList />
+    </MainContainer>
+  )
+}
+
+export default AwardsPage;
